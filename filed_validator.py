23ea5485e1362a9d4672dbb515ffe1e03a7d3339
Sarang-Bangade <bangadesarang@gmail.com>
2026-02-11 23:40:09 +0530

    filed_validation

--- filed_validator.py
+++ filed_validator.py
@@ -1,16 +1,28 @@
 # Field validation in pydantic 
-from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator
 from typing import List , Dict , Optional, Annotated
 
 class Patient(BaseModel):
 
     name:str
     email:EmailStr
-    age : int
+    age : int 
     weight : float
     married :bool
     allergies : List[str]
     contact_details : Dict[str,str]
+
+    @field_validator('email')
+    @classmethod
+    def email_validator(cls,value):    
+        valid_domains = ['hdfc.com', 'icici.com']
+        #abc@gmail.com
+        domain_name = value.split('@')[-1]
+
+        if domain_name not in valid_domains:
+            raise ValueError('Not a valid domain')
+            
+        return value    
 
 def insert_patient_data(patient:Patient):
     print(patient.name)
@@ -24,7 +36,7 @@ def update_patient_data(patient:Patient):
     print(patient.allergies)
     print('updated')    
 
-patient_info = {'name':'Sarang','email':'abc@gmail.com' ,'age':23, 'weight':'75.2','married':True, 'allergies':[], 'contact_details':{'email':'abc@gmail.com','phone':'23456789'}}
+patient_info = {'name':'Sarang','email':'abc@hdfc.com' ,'age':23, 'weight':'75.2','married':True, 'allergies':[], 'contact_details':{'email':'abc@gmail.com','phone':'23456789'}}
 
 patient1 = Patient(**patient_info)
 
