7a061108d18ec69878bd0ed84e6f8b996ed7f0e3
Sarang-Bangade <bangadesarang@gmail.com>
2026-02-13 22:55:49 +0530

    filed_validation

--- filed_validator.py
+++ filed_validator.py
@@ -2,6 +2,7 @@
 from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator
 from typing import List , Dict , Optional, Annotated
 
+
 class Patient(BaseModel):
     name:str
     email:EmailStr
